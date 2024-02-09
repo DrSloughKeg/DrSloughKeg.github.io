689f2918e6f3bf1e3162471218860c4f2d2c0542
DrSloughKeg <bpye1995@gmail.com>
2024-02-09 14:45:29 -0500

    fixed page imports, added basic css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+import Resume from "./pages/resume";
 
 function App() {
   return (
@@ -10,13 +14,13 @@ function App() {
           <Link to="/projects">Projects</Link>
           <Link to="/resume">Resume</Link>
           <Link to="/contact">Contact</Link>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
         </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </Router>
     </div>
   );

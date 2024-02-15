ccc3c0db32498d2dd209d07c589ea1d8982a8a35
DrSloughKeg <bpye1995@gmail.com>
2024-02-15 11:16:24 -0500

    started contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_rsk4mkp", "template_2ot3tq4", form.current, {
+        publicKey: "SI1c4ahLxvm3Teif8",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <div className="mainBox">
       <h1>Contact</h1>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 }

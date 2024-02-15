bbdc865d0d2038b053faf2ec11ae4da6a354a05b
DrSloughKeg <bpye1995@gmail.com>
2024-02-15 14:54:02 -0500

    small screen css fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,6 @@ import React from "react";
 function Home() {
   return (
     <div className="mainBox">
-      <br />
-      <br />
-      <br />
-      <br />
       <p className="textblock">
         Hi, My name is Benjamin Pye. I'm a Web Developer and Programmer living
         in Montreal. I'm trained in both front and back-end development. I love
